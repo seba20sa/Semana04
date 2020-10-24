fe535ba9e874f5a679d28ef9e4a96ca398f7af77
Sebastian Sileoni <seba20sa@gmail.com>
2020-10-24 10:46:14 -0300

    added ej3

--- js/ej1.js
+++ js/ej1.js
@@ -1,17 +1,17 @@
-/* EJ-1 Variables y Operadores 
-A)Crear dos variables numéricas y utilizar el operador suma para guardar el 
-valor de la suma de ambos números en una 3er variable.*/
+//EJ-1 Variables y Operadores 
+//A) Crear dos variables numéricas y utilizar el operador suma para guardar el 
+// valor de la suma de ambos números en una 3er variable.
 var a = 2;
 var b= 7;
 var c = a + b;
 console.log(c);
-/*B)Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+//B) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 var strA = 'Hola';
 var srtB = 'gente.';
 var srtC = strA + ' ' + srtB;
 console.log(srtC);
-/*C)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del
-     string) guardando el resultado la suma una 3er variable (utilizar length).*/
+//C) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del
+// string) guardando el resultado la suma una 3er variable (utilizar length).*/
 var strFirst = 'Android';
 var strSecond = 'Apple';
 var srtLengthSum = strFirst.length + strSecond.length;
---
+++ js/ej3.js
@@ -0,0 +1,30 @@
+// Ej 3 Arrays
+// A) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+//"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola
+// los meses 5 y 11 (utilizar console.log)
+var year =  ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+    "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(year[4]);
+console.log(year[10]);
+//B) Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+console.log(year.sort());
+//C) Agregar un elemento al principio y al final del array (utilizar unshift y push).
+year =  ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+    "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+year.push('Decembro');
+year.unshift('Janeiro');
+console.log(year);
+//D) Quitar un elemento del principio y del final del array (utilizar shift y pop).
+year.shift();
+year.pop();
+console.log(year);
+//E) Invertir el orden del array (utilizar reverse)
+year.reverse();
+console.log(year);
+//F) Unir todos los elementos del array en un único string donde cada mes
+// este separado por un guión - (utilizar join).
+var joinedYear = year.reverse().join('-');
+console.log(joinedYear);
+//G) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+var slicedYear = year.slice(4,11);
+console.log(slicedYear); 
